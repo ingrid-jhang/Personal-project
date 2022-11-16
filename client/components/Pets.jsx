8f7bd8b748389d01e60f75943e36ce2b47eb1b31
ingrid-jhang <112028158+ingrid-jhang@users.noreply.github.com>
2022-11-16 13:05:01 +1300

    add select bar

--- client/components/Pets.jsx
+++ client/components/Pets.jsx
@@ -3,23 +3,44 @@ import { Link } from 'react-router-dom'
 import { getPets } from '../apiClient'
 
 const Pets = () => {
-  const [petsData, setPrtsData] = useState([])
-
+  const [petsData, setPetsData] = useState([])
+  const [type, setType] = useState('')
   useEffect(() => {
     getPets()
       .then((pets) => {
         console.log(pets)
-        setPrtsData(pets)
+        setPetsData(pets)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
+  function filterPet(pet) {
+    if (!type) {
+      return true
+    }
+    return pet.type_id == type
+  }
+  const filterPets = petsData.filter(filterPet)
+  console.log(filterPets)
+
+  function handleChange(e) {
+    setType(e.target.value)
+  }
+
+  //HANDCHANGE
   return (
     <>
+      <label htmlFor="pet-select">Choose a pet:</label>
+      <select name="pets" id="pet-select" onChange={handleChange}>
+        <option value="">--Please choose an option--</option>
+        <option value="">All Pets</option>
+        <option value="2">Dog</option>
+        <option value="1">Cat</option>
+      </select>
       <div className="pets-container">
-        {petsData.map(({ id, name, breed, age, location, image }) => {
+        {filterPets.map(({ id, name, breed, age, location, image }) => {
           return (
             <Link to={`/pets/${id}`} key={id}>
               <div key={id}>
